df1da6c5336ed444f9b78793f005f131d11369a2
Hedgehogcnm <159747001+Hedgehogcnm@users.noreply.github.com>
2025-09-13 18:43:12 +0800

    Initialize VB & DB Connection

---
+++ Sales and Inventory Management System/DBConnection.vb
@@ -0,0 +1,11 @@
+﻿Imports MySql.Data.MySqlClient
+Module DBConnection
+    Public conn As New MySqlConnection("server=localhost; user id=root; password=; database=db_sales_inventory_management_system;")
+
+    '#To Open Database Connection
+    'conn.Open() 
+
+    '#To Close Database Connection
+    'conn.Close()
+
+End Module
---
+++ Sales and Inventory Management System/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            conn.Open()
+            MessageBox.Show("Database Connected!")
+            conn.Close()
+        Catch ex As Exception
+            MessageBox.Show("Connection failed: " & ex.Message)
+        End Try
+    End Sub
+End Class
